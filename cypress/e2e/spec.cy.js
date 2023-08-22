3532f40622ee732c4c1fe97a163717007af4d7ea
Angelo Fernandes Machado Fonseca <angelofmfonseca@gmail.com>
2023-08-22 14:54:19 -0300

    feat: cypress.config baseUrl

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,6 @@
-describe("template spec", () => {
-  it("visits the homepage", () => {
-    cy.visit("http://localhost:3000/fundamentals");
+describe("Fundamentals tests", () => {
+  it("Contains the correct header text", () => {
+    cy.visit("/fundamentals");
     cy.get("[data-test='fundamentals-header']").should(
       "contain.text",
       "Testing Fundamentals",
