a48ca4ccada91dca94137acb67cc46a993070417
Angelo Fernandes Machado Fonseca <angelofmfonseca@gmail.com>
2023-08-22 15:51:50 -0300

    feat: adding beforeEach()

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,13 +1,15 @@
 describe("Fundamentals tests", () => {
-  it("Contains the correct header text", () => {
+  beforeEach(() => {
     cy.visit("/fundamentals");
+  });
+
+  it("Contains the correct header text", () => {
     cy.get("[data-test='fundamentals-header']").should(
       "contain.text",
       "Testing Fundamentals",
     );
   });
   it("The accordion works correctly", () => {
-    cy.visit("/fundamentals");
     cy.contains(/Your tests will exist in a describe block/i).should(
       "not.be.visible",
     );
