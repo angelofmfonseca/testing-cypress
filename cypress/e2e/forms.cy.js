4c5d8ef8afeb9c9a642eeaee8def2f099fe36260
Angelo Fernandes Machado Fonseca <angelofmfonseca@gmail.com>
2023-08-23 15:06:45 -0300

    feat: alias + invalid email subscribe test

--- cypress/e2e/forms.cy.js
+++ cypress/e2e/forms.cy.js
@@ -11,9 +11,18 @@ describe("Form tests", () => {
     cy.getDataTest("email-input")
       .find("input")
       .should("have.attr", "type", "email")
-      .type("hello@cypress.com");
+      .as("subscribe-input");
+    cy.get("@subscribe-input").type("hello@cypress.com");
     cy.contains(/Successfully subbed: hello@cypress.com/i).should("not.exist");
     cy.getDataTest("subscribe-button").click();
     cy.contains(/Successfully subbed: hello@cypress.com/i).should("exist");
+
+    cy.wait(3000);
+
+    cy.contains(/Successfully subbed: hello@cypress.com/i).should("not.exist");
+    cy.contains(/Invalid email: hello@cypress.io!/i).should("not.exist");
+    cy.get("@subscribe-input").type("hello@cypress.io");
+    cy.getDataTest("subscribe-button").click();
+    cy.contains(/Invalid email: hello@cypress.io!/i).should("exist");
   });
 });
